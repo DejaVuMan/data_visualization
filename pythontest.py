f692647494b405e824932618a33bc85925698f3f
DejaVuMan <sobolewskip88@gmail.com>
2020-02-28 14:27:40 +0100

    Added list of Python types.

--- pythontest.py
+++ pythontest.py
@@ -5,3 +5,10 @@ def main():
     pass
 
 #Comments start with Hashtags!
+name = "Doge Charger"
+print(name)
+print(type(name))
+print(type(5))
+print(type(5.7))
+print(type(True))
+print(type(None))
