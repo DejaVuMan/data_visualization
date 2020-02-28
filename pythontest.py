1ab52a323317991eb1b478977106adda1cf8256e
DejaVuMan <sobolewskip88@gmail.com>
2020-02-28 14:35:00 +0100

    Added Name changes in two different ways and prints that change.

--- pythontest.py
+++ pythontest.py
@@ -12,3 +12,9 @@ print(type(5))
 print(type(5.7))
 print(type(True))
 print(type(None))
+print(name[2])
+#name[2] = "G" #You are unable to mutate this type to a different type!
+name = "Alan Szuszkiewicz"
+name=name.lower()
+#print(name.lower())
+print(name)
