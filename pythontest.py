37614f16f81ff8118fd20232611efe8357710a6c
DejaVuMan <sobolewskip88@gmail.com>
2020-02-28 14:45:43 +0100

    Added example of multiple ways of how to type in variable name and ages in a printed string.

--- pythontest.py
+++ pythontest.py
@@ -15,6 +15,20 @@ print(type(None))
 print(name[2])
 #name[2] = "G" #You are unable to mutate this type to a different type!
 name = "Alan Szuszkiewicz"
-name=name.lower()
+name=name.lower() #This is called a chain
 #print(name.lower())
 print(name)
+age = 18
+#print(name + " jest z Stanow Zjednoczonych i ma "+ age +"lat.")
+# ^ incorrect inseration of int into string
+#print(name + " jest z Stanow Zjednoczonych i ma " + age.__str__() + " lat.")
+# You could also use str(age). This isn't the most efficient method, though.
+
+print("{} jest z Stanow Zjednoczonych i ma {} lat.".format(name,age))
+#This is the better method - easy to use, less text to type.
+print("{0} jest z Stanow Zjednoczonych i ma {1} lat.".format(name,age))
+#This method also works!
+print(f"{name} jest z Stanow Zjednoczonych i ma {age} lat.")
+#f-string is generally regarded as the best method - from 3.6 onward.
+
+
