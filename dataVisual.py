fe3e469120702c47c8b21f352564fa915150e510
Alan S <szuszkiewiczalan928@gmail.com>
2020-03-09 08:14:55 +0100

    Added Direct File Path to .csv File

--- dataVisual.py
+++ dataVisual.py
@@ -1,4 +1,4 @@
 import pandas as pd
 #import nump as np
-df = pd.read_csv('temporal.csv')
+df = pd.read_csv('file:///C:/GitRepo/data_visualization/temporal.csv')
 df.head(10) #view first 10 data rows
