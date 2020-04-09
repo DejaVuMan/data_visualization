6406f58f9672156a5fca2c47f74bc421185bcbc7
Alan S <szuszkiewiczalan928@gmail.com>
2020-04-09 13:18:01 +0200

    Began work on Ex 4
    general layout in word form before beginning creation of code

---
+++ classwork+homework/04-06-2020 Lab4/2_OOP_Intro/Ex4_GroceriesClasses.py
@@ -0,0 +1,10 @@
+# This program creates the class ForGroceries, which contains:
+# product_name, amount, unitMeasure, price
+# and also methods:
+#   constructor - which gives values
+#   show_price() - list info on price in console
+#   hm_product() - info on how much of a given item there is, i.e:
+#       quantity+unit_measurements - ex: 1 item, 3lbs, etc.
+#   hm_cost() - counts how much an amount of product costs, i.e:
+#       3lbs of potatoes, and the price is $1.50 per pound, so the function returns
+#       1.50*3 = 4.50.
