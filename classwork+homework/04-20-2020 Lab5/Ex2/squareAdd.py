ea5babd3c3aabf202c3d605f2d56d46b7ab4f855
Alan S <szuszkiewiczalan928@gmail.com>
2020-04-26 16:36:20 +0200

    Updated Ex2
    Proper DunderAdd implementation

--- classwork+homework/04-20-2020 Lab5/Ex2/squareAdd.py
+++ classwork+homework/04-20-2020 Lab5/Ex2/squareAdd.py
@@ -1,16 +1,20 @@
-class Square():
+class Square:
 
     def __init__(self, x):
         self.x = x
         self.y = x
 
-#    def __add__(self, y):
-#       return self.x + self.y
+    def __repr__(self):
+        return "Square({})".format(self.x)
 
     def __str__(self):
-        #return 'This square has a side length of {} units'.format(self.x)
-        return 'The length of the new side length is {} units'.format(self.x.__add__(self.y))
+        return 'The length of the new side length is {self.x} units'.format(self=self)
+    
+    def __add__(self, other):
+        return self.x + self.y
+
     
 
 sq = Square(5)
 print(sq)
+print(sq+sq)
